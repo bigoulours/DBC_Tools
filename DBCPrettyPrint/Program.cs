106fe3da623c8d4418f4c2bda4963d305b30df69
Brian Manthos <--unset>
2018-12-23 21:08:01 -0800

    add DBCPrettyPrint tool

---
+++ DBCPrettyPrint/Program.cs
@@ -0,0 +1,51 @@
+﻿using DBCLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DBCPrettyPrint
+{
+  class Program
+  {
+    static string outputDirectory = "Pretty";
+    static bool overwriteExisting = true;
+
+    static void Main(string[] args)
+    {
+      DBCLib.Reader dbcReader = new Reader();
+
+      if (!Directory.Exists(outputDirectory))
+      {
+        Directory.CreateDirectory(outputDirectory);
+      }
+
+      IEnumerable<string> files = Directory.EnumerateFiles(".", "*.dbc");
+      foreach (string fileSrc in files)
+      {
+        string fileDst = string.Format(@"{0}\{1}", outputDirectory, Path.GetFileName(fileSrc));
+        if (overwriteExisting || !File.Exists(fileDst) || (new System.IO.FileInfo(fileDst).Length == 0))
+        {
+          Console.Title = fileSrc;
+          Console.WriteLine("R {0}", fileSrc);
+          List<object> entries = dbcReader.Read(fileSrc);
+
+          if (entries != null)
+          {
+            Console.WriteLine("W {0}", fileDst);
+            using (Stream streamDst = new FileStream(fileDst, FileMode.Create, FileAccess.Write, FileShare.Read))
+            {
+              using (StreamWriter streamWriter = new StreamWriter(streamDst, Encoding.Default))
+              {
+                foreach (object entry in entries)
+                {
+                  ((Entry)entry).WriteDBC(streamWriter);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
