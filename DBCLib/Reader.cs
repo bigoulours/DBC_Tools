79dd3f454663054729438e6a0807ef30ba60ae29
Brian Manthos <--unset>
2018-12-25 22:06:40 -0800

    DBCLib - use InvariantCulture for reading

--- DBCLib/Reader.cs
+++ DBCLib/Reader.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace DBCLib
 {
@@ -53,107 +55,117 @@ namespace DBCLib
     {
       List<object> entries = new List<object>();
 
-      bool errors = false;
-      bool exceptionThrown = false;
+      var currentCulture = Thread.CurrentThread.CurrentCulture;
+      Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
-      uint numLines = 0;
-      if (!streamReader.EndOfStream)
+      try
       {
-        string line = streamReader.ReadLine();
-        numLines++;
+        bool errors = false;
+        bool exceptionThrown = false;
 
-        do
+        uint numLines = 0;
+        if (!streamReader.EndOfStream)
         {
-          bool parsed = false;
-          try
-          {
-            if (line.Trim().Length > 0)
-            {
-              parsed =
-                TryParse<DBCLib.AttributeValue>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<AttributeDefinition>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<AttributeDefault>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<BitTiming>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<Comment>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<Message>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<MessageTransmitters>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<NewSymbols>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<Nodes>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<Value>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<ValueTable>(ref line, ref numLines, streamReader, entries) ||
-                TryParse<DBCLib.Version>(ref line, ref numLines, streamReader, entries)
-                ;
+          string line = streamReader.ReadLine();
+          numLines++;
 
-              if (!parsed)
+          do
+          {
+            bool parsed = false;
+            try
+            {
+              if (line.Trim().Length > 0)
               {
-                errors = true;
-                Console.WriteLine("E {0}({1}): {2}",
+                parsed =
+                  TryParse<DBCLib.AttributeValue>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<AttributeDefinition>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<AttributeDefault>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<BitTiming>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<Comment>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<Message>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<MessageTransmitters>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<NewSymbols>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<Nodes>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<Value>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<ValueTable>(ref line, ref numLines, streamReader, entries) ||
+                  TryParse<DBCLib.Version>(ref line, ref numLines, streamReader, entries)
+                  ;
+
+                if (!parsed)
+                {
+                  errors = true;
+                  Console.WriteLine("E {0}({1}): {2}",
+                    path,
+                    numLines,
+                    line
+                    );
+                }
+              }
+            }
+            catch (Exception e)
+            {
+              exceptionThrown = true;
+              Console.WriteLine(e.ToString());
+              Console.WriteLine(e.StackTrace);
+              Console.WriteLine("X {0}({1}): {2}",
+                path,
+                numLines,
+                line
+                );
+              break;
+            }
+            finally
+            {
+              if (exceptionThrown)
+              {
+                Console.WriteLine("{0}({1}): {2}",
                   path,
                   numLines,
                   line
                   );
               }
             }
-          }
-          catch (Exception e)
-          {
-            exceptionThrown = true;
-            Console.WriteLine(e.ToString());
-            Console.WriteLine(e.StackTrace);
-            Console.WriteLine("X {0}({1}): {2}",
-              path,
-              numLines,
-              line
-              );
-            break;
-          }
-          finally
-          {
-            if (exceptionThrown)
+
+            if (!parsed)
             {
-              Console.WriteLine("{0}({1}): {2}",
-                path,
-                numLines,
-                line
-                );
+              line = null;
+              if (!streamReader.EndOfStream)
+              {
+                line = streamReader.ReadLine();
+                numLines++;
+              }
             }
-          }
+          } while (line != null);
+        }
 
-          if (!parsed)
-          {
-            line = null;
-            if (!streamReader.EndOfStream)
-            {
-              line = streamReader.ReadLine();
-              numLines++;
-            }
-          }
-        } while (line != null);
-      }
-
-      if (exceptionThrown || (errors && !AllowErrors))
-      {
-        return null;
-      }
-
-      if (entries.Count == 0)
-      {
-        return null;
-      }
-
-      if (DisplayEntries)
-      {
-        foreach (object entry in entries)
+        if (exceptionThrown || (errors && !AllowErrors))
         {
-          Console.WriteLine("D {0}", entry.ToString());
-          if (entry is Message)
+          return null;
+        }
+
+        if (entries.Count == 0)
+        {
+          return null;
+        }
+
+        if (DisplayEntries)
+        {
+          foreach (object entry in entries)
           {
-            foreach (Message.Signal signal in ((Message)entry).Signals)
+            Console.WriteLine("D {0}", entry.ToString());
+            if (entry is Message)
             {
-              Console.WriteLine("D  {0}", signal.ToString());
+              foreach (Message.Signal signal in ((Message)entry).Signals)
+              {
+                Console.WriteLine("D  {0}", signal.ToString());
+              }
             }
           }
         }
+      }
+      finally
+      {
+        Thread.CurrentThread.CurrentCulture = currentCulture;
       }
 
       return entries;
